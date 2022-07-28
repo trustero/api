4c6a5086011a2fb38715603f83448475d98d82a6
Phillip Liu <phillip@ntrce.co>
2022-07-27 19:42:53 -0700

    fix doc path

--- doc/proto-gen-doc.sh
+++ doc/proto-gen-doc.sh
@@ -5,7 +5,7 @@ which protoc > /dev/null || { \
   exit 1; \
 }
 
-[ -z ${DOC_DIR+x} ] && DOC_DIR="./md"
+[ -z ${DOC_DIR+x} ] && DOC_DIR="."
 mkdir -p $DOC_DIR
 
 [ -z ${PROTOS_SRC_DIR+x} ] && PROTOS_SRC_DIR="../proto"
@@ -15,11 +15,13 @@ export PATH=$PATH:$(go env GOPATH)/bin
 
 for pkg in $PROTO_PACKAGES; do
   proto_files=$(find ${PROTOS_SRC_DIR}/"${pkg}" -name "*.proto")
-
+  doc_dir=${DOC_DIR}/${pkg}
+  mkdir -p ${doc_dir}
   for proto in $proto_files; do
+    proto_prefix=${proto%".proto"}
     protoc \
-          --doc_out=$DOC_DIR \
-          --doc_opt="markdown,${pkg}.md" \
+          --doc_out=${doc_dir} \
+          --doc_opt="markdown,${proto_prefix}.md" \
           --proto_path="${PROTOS_SRC_DIR}" \
           -I "${PROTOS_SRC_DIR}" \
           "${proto}"
