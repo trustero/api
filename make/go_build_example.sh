6d41a1ed7954f36b9ca598ceba1d87129aa718af
Phillip Liu <phillip@ntrce.co>
2022-08-03 19:47:50 -0700

    remove uneccessary comments.

--- make/go_build_example.sh
+++ make/go_build_example.sh
@@ -6,6 +6,6 @@ DESTDIR=${SCRIPT_DIR}/../build
 for file in "$@"
 do
     EXE=$(basename "$(dirname "$file")")
-    go build -o "$DESTDIR"/"$EXE" "$file" ;\
-        [ -e "$DESTDIR"/"$EXE" ] && chmod 755 "$DESTDIR"/"$EXE"
+    go build -o "$DESTDIR"/"$EXE" "$file"
+    [ -e "$DESTDIR"/"$EXE" ] && chmod 755 "$DESTDIR"/"$EXE"
 done
