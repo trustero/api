a9169d8722bfb8e2d88c85e8a8107ec4f8c0da5c
Phillip Liu <phillip@ntrce.co>
2022-08-15 16:39:06 -0700

    generate proto docs

--- make/license_header.sh
+++ make/license_header.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Note that we need to have extra blank lines after the license comment header
+# to prevent go doc from thinking the license header is package documentation.
 read -r -d '' header << EOF
 // This file is subject to the terms and conditions defined in
 // file 'LICENSE.txt', which is part of this source code package.
+
 
 EOF
 
