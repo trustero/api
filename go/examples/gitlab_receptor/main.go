6d9b142ae571d6b78e994123e1e2c34ba6e4dc86
Ankit Patel <ankit@trustero.com>
2022-08-23 23:20:01 -0700

    add better comments to gitlab example

--- go/examples/gitlab_receptor/main.go
+++ go/examples/gitlab_receptor/main.go
@@ -25,6 +25,11 @@ type GitLabUser struct {
 	LastActivityOn   *time.Time `trustero:"display:Last Activity On;order:7"`
 }
 
+// This struct holds the credentials the receptor needs to authenticate with the
+// service provider. A display name and placeholder tag should be provided
+// which will be used in the UI when activating the receptor.
+// This is what will be returned in the GetCredentialObj call
+
 // Receptor defines the GitLab service credentials required for connecting to the GitLab
 // service and gathering necessary evidence to support its use.
 type Receptor struct {
@@ -39,21 +44,32 @@ const (
 )
 
 // GetReceptorType implements the [receptor_sdk.Receptor] interface.
+// Set the name of the receptor in the const declaration above
+// This will let the receptor inform Trustero about itself
 func (r *Receptor) GetReceptorType() string {
 	return "gitlab_receptor"
 }
 
 // GetKnownServices implements the [receptor_sdk.Receptor] interface.
+// Set the names of the services in the const declaration above
+// This will let the receptor inform Trustero about itself
+// Feel free to add or remove services as needed
 func (r *Receptor) GetKnownServices() []string {
 	return []string{serviceName}
 }
 
 // GetCredentialObj implements the [receptor_sdk.Receptor] interface.
+// This will return Receptor struct defined above when the receptor is asked to
+// identify itself
 func (r *Receptor) GetCredentialObj() (credentialObj interface{}) {
 	return r
 }
 
 // Verify implements the [receptor_sdk.Receptor] interface.
+// This function will call into the service provider API with the provided 
+// credentials and confirm that the credentials are valid. Usually a simple
+// API call like GET org name. If the credentials are not valid, 
+// return a relevant error message
 func (r *Receptor) Verify(credentials interface{}) (ok bool, err error) {
 	c := credentials.(*Receptor)
 	ok = true
@@ -68,6 +84,14 @@ func (r *Receptor) Verify(credentials interface{}) (ok bool, err error) {
 }
 
 // Discover implements the [receptor_sdk.Receptor] interface.
+// The Discover function returns a list of Service Entities. This function 
+// makes any relevant API calls to the Service Provider to gather information
+// about how many Service Entity Instances are in use. If at any point this 
+// function runs into an error, log that error and continue
+
+// In this example, Discover is making a query to GET the group name. 
+// The group name is then added to a list of Service Entities that will be
+// returned to the Trustero platform to display in the UI.
 func (r *Receptor) Discover(credentials interface{}) (svcs []*receptor_v1.ServiceEntity, err error) {
 	c := credentials.(*Receptor)
 	var git *gitlab.Client
@@ -83,6 +107,16 @@ func (r *Receptor) Discover(credentials interface{}) (svcs []*receptor_v1.Servic
 }
 
 // Report implements the [receptor_sdk.Receptor] interface.
+// Report will often make the same API calls made in the Discover call, but it
+// will additionally create evidences with the data returned from the API calls
+
+// In this example, Report makes a query to GET all group members in the account
+// Each member in the list of members is converted into a GitLabUser which is 
+// defined above. Each converted member is then added as a Row into an evidence
+// The Evidence also has a caption and a description that will be used in the
+// Trustero UI. 
+// NOTE: The caption is prepended with the service name, so in the UI, it will
+// read: "GitLab Group Members"
 func (r *Receptor) Report(credentials interface{}) (evidences []*receptor_sdk.Evidence, err error) {
 	c := credentials.(*Receptor)
 	report := receptor_sdk.NewReport()
---
+++ go/examples/gitlab_receptor/trr-gitlab.md
@@ -0,0 +1,21 @@
+// These are instructions on how to generate the needed credentials for this
+// receptor. 
+// GitLab receptor needs a GroupID and a Token to access an account.
+
+
+First, get the group ID
+1. In the top-left corner, select **Menu**.
+2. Select **Groups**.
+3. Select **Your groups**.
+4. Select a group.
+5. Copy the `group ID` under the **group name**.
+6. Paste the `group ID` into the **Group ID** field.
+
+Next, create a read-only personal access token
+1. In the top-right corner, select your avatar.
+2. Select **Edit** profile.
+3. On the left sidebar, select **Access Tokens**.
+4. Enter a name and optional expiry date for the token.
+5. Select `read_api`, `read_user`, and `read_repository` scopes.
+6. Select **Create personal access token**.
+7. Enter the personal access token into the **Token** field.
