2e4cb622c541e248916204bd392b88bf1e09a117
David <david@trustero.com>
2023-07-26 18:09:36 -0500

    created proto-gen.sh script to generate js proto bindings and a package.json in order to publish the repo as a yarn package.

---
+++ proto-gen.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+which protoc > /dev/null || {
+  echo "error: protoc not installed" >&2
+  exit 1
+}
+
+set -euo pipefail
+
+cd $(dirname "$0")
+
+codegen_dir="./lib"
+index_js="./index.js"
+> $index_js
+
+rm -rf ${codegen_dir}
+mkdir ${codegen_dir}
+
+proto_location="$(
+  pwd
+)/proto/receptor_v1"
+
+proto_files=$(find "${proto_location}" -name '*.proto')
+
+for proto_file in $proto_files; do
+  protoc "${proto_file}" \
+    -I="${proto_location}" \
+    --js_out=import_style=commonjs,binary:"${codegen_dir}" \
+    --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:"${codegen_dir}" \
+    --experimental_allow_proto3_optional
+
+  protoc "${proto_file}" \
+    -I="${proto_location}" \
+    --js_out=import_style=typescript,binary:"${codegen_dir}" \
+    --grpc-web_out=import_style=commonjs+dts,mode=grpcwebtext:"${codegen_dir}" \
+    --experimental_allow_proto3_optional
+done
+
+js_files=$(find "${codegen_dir}" -name '*.js' | tr '\n' ' ')
+
+for js_file in $js_files; do
+  echo "export * from \"${js_file}\"" >> ${index_js}
+done
