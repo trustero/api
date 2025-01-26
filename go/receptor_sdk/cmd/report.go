cb8788fc244e2b5e73c356d8d0c700eb1af7b5df
sen-trustero <sen@trustero.com>
2025-01-26 11:24:02 -0800

    remove do no duplicate

--- go/receptor_sdk/cmd/report.go
+++ go/receptor_sdk/cmd/report.go
@@ -78,6 +78,8 @@ func reportEvidence(rc receptor_v1.ReceptorClient, finding *receptor_v1.Finding,
 			ServiceAccountId: evidence.ServiceAccountId,
 			Controls:         evidence.Controls,
 			IsManual:         evidence.IsManual,
+			RelevantDate:     &evidence.RelevantDate,
+			DoNotDuplicate:   evidence.DoNotDuplicate,
 		}
 
 		if evidence.Document != nil { // evidence is a blob and/or path to blob
